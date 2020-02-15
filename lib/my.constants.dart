347c852b1007335e635be55a3d51a5641d1ae585
Weslley Maciel <weslley.maciel.c@gmail.com>
2020-02-15 20:41:33 -0300

    MB-3-Created launch screen

--- lib/my.constants.dart
+++ lib/my.constants.dart
@@ -6,9 +6,9 @@ class MyConstants extends InheritedWidget {
   const MyConstants({Widget child, Key key}): super(key: key, child: child);
 
   final String successMessage = 'Some message';
-  final double textLarge = 20;
-  final double textMedium = 15;
-  final double textSmall = 12;
+  final double textLarge = 16;
+  final double textMedium = 13;
+  final double textSmall = 10;
 
   final double btnPaddingMedium = 10;
 
---
+++ lib/pages/register.page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
